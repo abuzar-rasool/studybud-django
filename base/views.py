f991b5124ea5f4efbf040d4fe72e4616a54ccf9f
Abuzar Rasool <abuzar@oraan.com>
2022-08-01 15:05:34 +0500

    half theme integration

--- base/views.py
+++ base/views.py
@@ -105,7 +105,7 @@ def loginPage(request):
 
 def logoutUser(request):
     logout(request)
-    return redirect('login')
+    return redirect('home')
 
 def register(request):
     page = 'register'
