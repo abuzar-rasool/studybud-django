fe2b7c2d521957c18cbf87613f5cd4c4fa34949c
Abuzar Rasool <abuzar@oraan.com>
2022-12-11 21:16:25 +0500

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-study-bud.herokuapp.com', '127.0.0.1']
 
@@ -167,4 +167,4 @@ SWAGGER_SETTINGS = {
     },
 }
 
-FORCE_SCRIPT_NAME = "/"
+# FORCE_SCRIPT_NAME = "/"
