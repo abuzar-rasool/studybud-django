dbac173f58d60cfb0716d938b087f1c9113e1e65
Abuzar Rasool <abuzar@oraan.com>
2022-12-11 20:38:37 +0500

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
